ea7db27ea3d003d196ab488346b2abe252caa414
Ben Mason <locutus@the-collective.net>
2022-07-05 00:23:18 -0400

    importer Cleanup

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,6 +1,4 @@
 from subprocess import call
-import pdfplumber
-import csv
 import os
 from logdownloaderv2 import pdf_table_extractor, write_csv_file
 
@@ -10,38 +8,10 @@ FILE_LOCATION = f"{HOME_DIR}/SynologyDrive/Drive/Documents/Police Logs"
 DEBUG = False
 MEDIA_LOG=False
 
-
-# def pdf_table_extractor(pdf_filename):
-
-#     pdf_data = pdfplumber.open(pdf_filename)
-#     call_list = []
-#     for pdf_page in pdf_data.pages:
-#         calls = pdf_page.extract_table()
-#         del(calls[0])           # remove page header
-#         call_list.extend(calls)
-
-
-#     if call_list[-1][0] == "Total calls:":
-#         total_calls = call_list[-1][1] 
-#         del(call_list[-1])
-#     else:
-#         print("Could not get total calls")
-#         total_calls = 0
-
-#     return call_list, int(total_calls)
-
-# def write_csv_file(csv_list, csv_file):
-
-#     with open(csv_file, 'w') as f:
-#         write = csv.writer(f)
-#         write.writerow(CSV_HEADER)
-#         write.writerows(csv_list)
-
 if MEDIA_LOG:
     base_file_path = f"{FILE_LOCATION}/Media Logs"
 else:
     base_file_path = f"{FILE_LOCATION}/Arrest Logs"
-
 
 def get_file_list(YEAR):
     file_list = os.listdir(base_file_path + '/' + YEAR)
