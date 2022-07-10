d54470aef4bf634ec75a49a9c70051091865dc39
Ben Mason <locutus@the-collective.net>
2022-07-09 23:18:00 -0400

    config update

--- config.py
+++ config.py
@@ -5,8 +5,11 @@ from os import environ
 
 MEDIA_LOG_URL = "https://www.portlandmaine.gov/472/Daily-Media-Logs"
 ARREST_LOG_URL = "https://www.portlandmaine.gov/471/Crime-in-Portland"
+# Support for Docker
 if not environ['FILE_LOCATION']:
     HOME_DIR = environ['HOME']
     FILE_LOCATION = f"{HOME_DIR}/SynologyDrive/Drive/Documents/Police Logs"
+else:
+    FILE_LOCATION =  environ['FILE_LOCATION']
 DEBUG = False
 LOAD_TIME_SLEEP = 10
