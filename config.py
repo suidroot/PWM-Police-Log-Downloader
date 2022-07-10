6eb4cb26ef9ad3e8ceb832e1b20dde33918f917c
Ben Mason <locutus@the-collective.net>
2022-07-09 23:13:42 -0400

    check env first

--- config.py
+++ config.py
@@ -3,9 +3,10 @@
 
 from os import environ
 
-HOME_DIR = environ['HOME']
 MEDIA_LOG_URL = "https://www.portlandmaine.gov/472/Daily-Media-Logs"
 ARREST_LOG_URL = "https://www.portlandmaine.gov/471/Crime-in-Portland"
-FILE_LOCATION = f"{HOME_DIR}/SynologyDrive/Drive/Documents/Police Logs"
+if not environ['FILE_LOCATION']:
+    HOME_DIR = environ['HOME']
+    FILE_LOCATION = f"{HOME_DIR}/SynologyDrive/Drive/Documents/Police Logs"
 DEBUG = False
 LOAD_TIME_SLEEP = 10
