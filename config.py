c50e67827f53b130d45374c13fe5cd00c3143a26
Ben Mason <locutus@the-collective.net>
2022-07-10 21:52:21 -0400

    Adding Discord Logging functionality

--- config.py
+++ config.py
@@ -2,14 +2,25 @@
 ''' Configuration for logdownloaderv2.py '''
 
 from os import environ
+import logging
 
 MEDIA_LOG_URL = "https://www.portlandmaine.gov/472/Daily-Media-Logs"
 ARREST_LOG_URL = "https://www.portlandmaine.gov/471/Crime-in-Portland"
 # Support for Docker
-if not environ['FILE_LOCATION']:
+try:
+    FILE_LOCATION =  environ['FILE_LOCATION']
+except KeyError:
     HOME_DIR = environ['HOME']
     FILE_LOCATION = f"{HOME_DIR}/SynologyDrive/Drive/Documents/Police Logs"
-else:
-    FILE_LOCATION =  environ['FILE_LOCATION']
+
 DEBUG = False
 LOAD_TIME_SLEEP = 10
+LOGGER_STREAM = '%(asctime)s - %(levelname)s - %(message)s'
+LOGGING_LEVEL = logging.INFO
+
+# Discord Logging
+ENABLE_DISCORD = True
+# set shell environment var for webhook URL
+DISCORD_WEBHOOK_URL = environ['DISCORD_PWM_WEBHOOK_URL']
+LOGGER_DISCORD = '%(message)s'
+DISCORD_NAME = "Logdl_bot"
--- logdownloaderv2.py
+++ logdownloaderv2.py
@@ -3,13 +3,12 @@
 
 # TODO: Directory Management and error handling
 
-# import os
 from os.path import exists
 from tempfile import TemporaryFile
 from time import sleep
 from datetime import datetime, timedelta
 import requests
-#import tabula   # tabula-py
+import logging
 import pdfplumber
 import csv
 from PyPDF2 import PdfReader
@@ -17,6 +16,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import config
+if config.ENABLE_DISCORD:
+    from discord_logging.handler import DiscordHandler
 
 DAYS_OF_WEEK = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
 MEDIA_CSV_HEADER = ['PD Call#', 'Call Start \nDate & Time', 'Call End \nDate & Time', 'Type of Call', 'Street Address / Location', 'Officer Name']
@@ -27,6 +28,29 @@ ARREST_LOG_URL = config.ARREST_LOG_URL
 DEBUG = config.DEBUG
 LOAD_TIME_SLEEP = config.LOAD_TIME_SLEEP
 FILE_LOCATION = config.FILE_LOCATION
+
+def setup_logger():
+    ''' Set up logging subsystem '''
+    logger = logging.getLogger()
+
+    if config.ENABLE_DISCORD:
+        discord_handler = DiscordHandler(
+            config.DISCORD_NAME, 
+            config.DISCORD_WEBHOOK_URL )
+
+        discord_format = logging.Formatter(config.LOGGER_DISCORD)
+        discord_handler.setFormatter(discord_format)
+        logger.addHandler(discord_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_format = logging.Formatter(config.LOGGER_STREAM)
+    stream_handler.setFormatter(stream_format)
+
+    # Add the handlers to the Logger
+    logger.addHandler(stream_handler)
+    logger.setLevel(config.LOGGING_LEVEL)
+
+    return logger
 
 def create_firefox_object(headless=True):
     ''' Create Firefox Object with option to disable headless mode '''
@@ -39,7 +63,6 @@ def create_firefox_object(headless=True):
         driver = webdriver.Firefox()
 
     return driver
-
 
 def get_arrest_log_url(headless=True):
     ''' Search for the URL of the Arrest Logs in webpage '''
@@ -69,10 +92,9 @@ def get_media_log_urls(days):
             day_url = link_element.get_attribute('href')
             url_data[day] = day_url
         except Exception as exception_info:
-            print("The error raised is: ", exception_info)
+            logger.error("The error raised is: ", exception_info)
 
-        if DEBUG:
-            print (f"{day}: {day_url}")
+        logger.debug(f"{day}: {day_url}")
 
     driver.quit()
 
@@ -110,7 +132,7 @@ def pdf_table_extractor(pdf_filename, media_log=True):
         del(calls[0])           # remove page header
         call_list.extend(calls)
 
-    # extract Total calls from PDf if unable to find print error
+    # extract Total calls from PDF if unable to find print error
     if call_list[-1][0] == "Total calls:":
         total_calls = call_list[-1][1] 
         del(call_list[-1])
@@ -122,10 +144,10 @@ def pdf_table_extractor(pdf_filename, media_log=True):
             total_calls = int(call_list[-1][0])
             del(call_list[-1])
         except ValueError:
-            print("Could not get total calls")
+            logger.error("Could not get total calls")
             total_calls = 0
     else:
-        print("Could not get total calls")
+        logger.error("Could not get total calls")
         total_calls = 0
 
     return call_list, int(total_calls)
@@ -158,66 +180,52 @@ def write_pdf_and_csv(meta_data, data, media_log=True):
         csv_filename = f"{FILE_LOCATION}/Arrest Logs/csv/{year_str}/arrestlog_{date_str}.csv"
 
     if not exists(pdf_new_filename):
-
-        if DEBUG:
-            print(f"writing {date_str} to {pdf_new_filename} ")
-        else:
-            print(" (pdf) ", end='')
+        logger.warning(f"writing {date_str} to {pdf_new_filename} ")
 
         with open(pdf_new_filename,'wb') as pdf_file:
             pdf_file.write(data)
     else:
-        if DEBUG:
-            print(f"Skipping writing {date_str} to {pdf_new_filename}")
-        else:
-            print(" (skip pdf) ", end='')
+        logger.info(f"Skipping writing {date_str} exists at {pdf_new_filename}")
+
 
     if not exists(csv_filename):
-        if DEBUG:
-            print(f"creating csv of {date_str} to {csv_filename} ")
-        else:
-            print(" (csv)")
+        logger.warning(f"Creating csv of {date_str} to {csv_filename} ")
+
         call_list, number_calls = pdf_table_extractor(pdf_new_filename, media_log=media_log)
         if len(call_list) != number_calls:
-            print(f"PDF Table Parsing Error: Call number mismatch {len(call_list)} != {number_calls}")
+            logger.error(f"PDF Table Parsing Error: Call number mismatch {len(call_list)} != {number_calls}")
         write_csv_file(call_list, csv_filename, media_log=media_log)
-        # tabula.convert_into(pdf_new_filename, csv_filename, pages='all')
     else:
-        if DEBUG:
-            print(f"Skipping csv of {date_str} to {csv_filename} ")
-        else:
-             print(" (skip csv)")
-
+        logger.info(f"Skipping csv of {date_str} exists at {csv_filename} ")
 
 def main():
     ''' Main function '''
-
-    print("Getting Media Log URLs")
+    
+    logger.warning("Getting Media Log URLs")
     url_data = get_media_log_urls(DAYS_OF_WEEK)
 
     for day in DAYS_OF_WEEK:
-        if DEBUG:
-            print(f"Downloading {day} : {url_data[day]} -", end='')
-        else:
-            print(f"Downloading {day} -", end='')
+        logger.info(f"Downloading {day}")
+        logger.debug(f"Downloading {day} : {url_data[day]}")
         status_code, content = download_content(url_data[day])
 
         if status_code == 200:
             meta_data = get_pdf_meta_data(content)
             date = meta_data['pdf_date'].strftime("%Y-%m-%d")
-            print(f' {date} ', end='')
+            logger.debug(f'PDF date: {date} ')
             write_pdf_and_csv(meta_data, content)
         else:
-            print(f"**** {day} file not found on server: {status_code} error ***")
+            logger.error(f"**** {day} file not found on server: {status_code} error ***")
 
-    print("Downloading Arrest Log: ", end='')
+    logger.warning("Starting Arrest Log")
     status_code, content = download_content(get_arrest_log_url())
 
     if status_code == 200:
         meta_data = get_pdf_meta_data(content)
         write_pdf_and_csv(meta_data, content, media_log=False)
     else:
-        print(f"{day} file not found")
+        logger.error(f"{day} file not found")
 
 if __name__ == '__main__':
+    logger = setup_logger()
     main()
